18f7fd361b3e3a34de291ceef123e85c22b482ca
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2024-03-11 15:22:39 +0800

    Perubahan ukuran komponen Button untuk Icon

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         sm: "h-9 rounded-md px-3",
         md: "h-10 px-4 py-2",
         lg: "h-11 rounded-md px-8",
-        icon: "size-10",
+        icon: "size-8",
       },
     },
     defaultVariants: {
