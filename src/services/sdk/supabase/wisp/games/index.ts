470aed649854560995d1ccf5ffd52cb60c519652
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2024-03-11 15:21:19 +0800

    Menambahkan panggilan SDK Supabase untuk Game terbaru

--- src/services/sdk/supabase/wisp/games/index.ts
+++ src/services/sdk/supabase/wisp/games/index.ts
@@ -1,6 +1,6 @@
 import { supabase } from "@services/sdk/supabase/wisp/config";
 
-async function getLatestGamesForCarousel() {
+async function getNewReleaseGamesForHeroCarousel() {
   const { data, error } = await supabase
     .from("Games")
     .select(
@@ -16,4 +16,18 @@ async function getLatestGamesForCarousel() {
   return data;
 }
 
-export { getLatestGamesForCarousel };
+async function getNewReleaseGames() {
+  const { data, error } = await supabase
+    .from("Games")
+    .select("id, name, grid: grid->>optimized")
+    .order("releaseDate", { ascending: false })
+    .limit(15);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export { getNewReleaseGamesForHeroCarousel, getNewReleaseGames };
